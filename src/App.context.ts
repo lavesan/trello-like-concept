04dbef76572f3b7d5ac73efb99d8bbb8867311ae
valdery <valdery.jur@gmail.com>
2020-05-10 23:35:14 -0300

    :sparkles: Adds the service to the AppContext
    Signed-off-by: valdery <valdery.jur@gmail.com>

--- src/App.context.ts
+++ src/App.context.ts
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { IBoard, ICard } from './views/home/home.interfaces';
+import BoardService from './services/board.service';
 
 export const AppContext = React.createContext({
-    draggedElem: {} as ICard,
+    draggedElem: {} as ICard | null,
     setDraggedElem: (param: ICard) => {},
     draggedPos: 0,
     setDraggedPos: (param: number) => {},
     boards: [] as IBoard[],
     setBoards: (param: IBoard[]) => {},
+    boardService: BoardService.getInstance(),
 });
