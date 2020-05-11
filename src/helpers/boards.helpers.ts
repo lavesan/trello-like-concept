20d67c0d45c10baa4f51a6b82c94ff5e24579abb
valdery <valdery.jur@gmail.com>
2020-05-11 13:23:18 -0300

    :nail_care: Sorts the boards and cards by their position
    Signed-off-by: valdery <valdery.jur@gmail.com>

--- src/helpers/boards.helpers.ts
+++ src/helpers/boards.helpers.ts
@@ -2,28 +2,47 @@ import { IMapIntoBoards } from './boards.interfaces';
 import { IBoardComponent } from '../components/board/board.interfaces';
 
 export const mapDataIntoBoards = ({ boards, tags, users, cards }: IMapIntoBoards): IBoardComponent[] => {
-    return boards.map(board => {
 
-        const filteredCards = cards.filter(card => card.boardId === board.id);
+  const sortByPosition = (elem: any, nextElem: any) => {
 
-        const mappedCards = filteredCards.map(card => {
+    if (elem.position < nextElem.position) {
+      return -1;
+    } else if (elem.position > nextElem.position) {
+      return 1;
+    }
+  
+    return 0;
 
-          const usersOnCard = users.filter(user => card.userIds.includes(user.id));
-          const tagsOnCard = tags.filter(tag => card.tagsIds.includes(tag.id));
+  }
 
-          return {
-            ...card,
-            index: 0,
-            users: usersOnCard,
-            tags: tagsOnCard,
-          }
+  const mappedBoards = boards.map(board => {
 
-        });
+    const filteredCards = cards.filter(card => card.boardId === board.id);
 
-        return {
-          ...board,
-          cards: mappedCards,
-        }
+    const mappedCards = filteredCards.map(card => {
 
-    })
+      const usersOnCard = users.filter(user => card.userIds.includes(user.id));
+      const tagsOnCard = tags.filter(tag => card.tagsIds.includes(tag.id));
+
+      return {
+        ...card,
+        index: 0,
+        users: usersOnCard,
+        tags: tagsOnCard,
+      }
+
+    });
+
+
+    const sortedCards = mappedCards.sort(sortByPosition);
+
+    return {
+      ...board,
+      cards: mappedCards,
+    }
+
+  })
+
+  return mappedBoards.sort(sortByPosition);
+
 }
