6b3035bb054c8d068e93a3317d03b21e1f91213f
valdery <valdery.jur@gmail.com>
2020-05-10 23:34:24 -0300

    :memo: Updates the models
    Signed-off-by: valdery <valdery.jur@gmail.com>

--- src/views/home/home.interfaces.ts
+++ src/views/home/home.interfaces.ts
@@ -1,10 +1,25 @@
 export interface ICard {
     id: number;
-    text: string;
+    title: string;
+    boardId: number;
+    userIds: number[];
+    position: number;
 }
 
 export interface IBoard {
     id: number;
     name: string;
     cards: ICard[];
+    position: number;
+}
+
+export interface ITag {
+    id: number;
+    name: string;
+}
+
+export interface IUser {
+    id: number;
+    name: string;
+    imgUrl: string;
 }
